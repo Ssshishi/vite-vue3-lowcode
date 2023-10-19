d4903213f3922d22e6c82e36f58ebdb1ee9aafa6
Cishy <51732370+Ssshishi@users.noreply.github.com>
2023-10-19 18:13:40 +0800

    Update index.vue

--- src/visual-editor/components/header/index.vue
+++ src/visual-editor/components/header/index.vue
@@ -1,12 +1,14 @@
 <template>
   <el-row type="flex" class="header">
+    
     <!--    左侧logo start-->
     <el-col :span="6" class="flex items-center">
       <div class="logo"></div>
       <h3 class="font-semibold">H5低代码</h3>
     </el-col>
     <!--    左侧logo end-->
-    <!--    中间操作页面部分 start-->
+    
+    <!--    中间操作页面部分 工具栏 start-->
     <el-col class="flex items-center" :span="12">
       <template v-for="(toolItem, toolIndex) in tools" :key="toolIndex">
         <div :class="[`w-1/${tools.length}`]" class="w-1/9">
@@ -23,6 +25,7 @@
       </template>
     </el-col>
     <!--    中间操作页面部分 end-->
+    
     <!--    右侧工具栏 start-->
     <el-col :span="6" class="right-tools flex flex-row-reverse items-center">
       <el-tooltip class="item" effect="dark" content="运行" placement="bottom">
@@ -35,11 +38,13 @@
           @click="runPreview"
         />
       </el-tooltip>
+      
       <!--      <el-tooltip class="item" effect="dark" content="github" placement="bottom">-->
       <!--        <a href="https://github.com/buqiyuan/vite-vue3-lowcode" target="_blank">-->
       <!--          <img :src="`${BASE_URL}github.svg`" width="40" height="40" alt="" />-->
       <!--        </a>-->
       <!--      </el-tooltip>-->
+      
       <el-popover placement="bottom" :width="140" trigger="hover">
         <el-row type="flex" class="row-bg" justify="space-around">
           <el-col :span="6">
@@ -63,6 +68,7 @@
       </el-popover>
     </el-col>
     <!--    右侧工具栏 end-->
+    
   </el-row>
   <preview v-model:visible="isShowH5Preview" />
 </template>
@@ -80,6 +86,7 @@
 
   const isShowH5Preview = ref(false);
 
+  // 工具
   const tools = useTools();
 
   const { jsonData } = useVisualData();
