9d239ee405d462e718a25bf121940e2aec335002
Cishy <51732370+Ssshishi@users.noreply.github.com>
2023-10-19 17:39:27 +0800

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
-import NProgress from 'nprogress'; // progress bar
+import NProgress from 'nprogress'; // progress bar Ajax'y 应用程序的细长进度条
 import 'nprogress/css/nprogress.css'; // 进度条样式
 
 NProgress.configure({ showSpinner: false }); // NProgress Configuration
@@ -13,14 +13,18 @@ const routes: Array<RouteRecordRaw> = [
 
 const router = createRouter({
   history: createWebHashHistory(),
+  // createWebHashHistory 创建一个 hash 模式的历史。在没有主机的 web 应用 (如 file://) 或无法通过配置服务器来处理任意 URL 的时候非常有用
   routes,
 });
 
+// 添加一个导航钩子，它会在每次导航之前被执行。返回一个用来移除该钩子的函数。
 router.beforeEach(() => {
   NProgress.start(); // start progress bar
   return true;
 });
 
+
+// 添加一个导航钩子，它会在每次导航之后被执行。返回一个用来移除该钩子的函数
 router.afterEach(() => {
   NProgress.done(); // finish progress bar
 });
