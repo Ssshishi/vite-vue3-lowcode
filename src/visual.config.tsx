5a7958f4f59d0b89931821b916bab78fca35f335
Cishy <51732370+Ssshishi@users.noreply.github.com>
2023-10-19 17:29:49 +0800

    Update visual.config.tsx

--- src/visual.config.tsx
+++ src/visual.config.tsx
@@ -2,11 +2,14 @@ import { createVisualEditorConfig } from './visual-editor/visual-editor.utils';
 import baseWidgets from '@/packages/base-widgets';
 import containerComponent from '@/packages/container-component';
 
+// 可视化配置
 export const visualConfig = createVisualEditorConfig();
+
 // 注册基础控件
 Object.entries(baseWidgets).forEach(([name, widget]) =>
   visualConfig.registry('baseWidgets', name, widget),
 );
+
 // 注册容器组件
 Object.entries(containerComponent).forEach(([name, widget]) =>
   visualConfig.registry('containerComponents', name, widget),
