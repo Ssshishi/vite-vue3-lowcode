c631cc0b49b184504d6a42a52f523c3a76035a10
Cishy <51732370+Ssshishi@users.noreply.github.com>
2023-10-20 11:20:05 +0800

    Update index.ts

--- src/visual-editor/components/left-aside/components/index.ts
+++ src/visual-editor/components/left-aside/components/index.ts
@@ -1,5 +1,8 @@
+// DefineComponent 提供类型推导的辅助函数
 import { DefineComponent } from 'vue';
 
+// 动态导入模块的方式
+// import.meta.globEager 匹配指定模式的模块路径，并一次性将这些模块导入并返回一个对象。
 const modules = import.meta.globEager('./*/index.(tsx|vue)');
 
 const components: Record<string, DefineComponent> = {};
@@ -10,6 +13,8 @@ for (const path in modules) {
   const comp = modules[path].default;
   components[comp.name || path.split('/')[1]] = comp;
 }
+
 console.log('left-aside components:', components);
 
 export default components;
+// 挺有意思的，用这种方式一并导出组件
